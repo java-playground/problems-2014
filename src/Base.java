e546cfe91c2cc8d6eda9b60675a0bd39cc76075a
Jonathan Lam <jlam55555@gmail.com>
2018-03-09 13:48:50 -0500

    updated base for brevity

--- src/Base.java
+++ src/Base.java
@@ -2,21 +2,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
 public class Base {
-
     public static void main(String[] args){
         try(BufferedReader br = new BufferedReader(new FileReader(new File("downloads/ExampleIO/Prob00.in.txt")))) { //change problem #
-
-            String line = null;
-
+            String line;
             while ((line = br.readLine()) != null){
 
                 /* Do things here */
 
 
             }
-
         } catch (IOException e){e.printStackTrace();}
     }
 }
